f56b6396b5a061202644ef65b8cbdb19258217e0
Iuliia Vlkv <yu.wlkw@gmail.com>
2022-07-22 11:02:17 +0200

    Initial Commit

---
+++ LearnByDoing/LearnByDoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnByDoingApp.swift
+//  LearnByDoing
+//
+//  Created by Iuliia Volkova on 22.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct LearnByDoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
